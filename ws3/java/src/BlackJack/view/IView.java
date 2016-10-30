660bfe35df1c465eba36dbb81d12626a6a370ff3
Tobias Nilsson <tn222gf@student.lnu.se>
2016-10-30 21:29:18 +0100

    changes to class diagram

--- ws3/java/src/BlackJack/view/IView.java
+++ ws3/java/src/BlackJack/view/IView.java
@@ -6,11 +6,11 @@ public interface IView
 {
   void DisplayWelcomeMessage();
   int GetInput();
-  void DisplayCard(BlackJack.model.Card a_card);
-  void DisplayPlayerCard(BlackJack.model.Card a_card);
-  void DisplayDealerCard(BlackJack.model.Card a_card);
-  void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
-  void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
+  void DisplayCard(Card a_card);
+  void DisplayPlayerCard(Card a_card);
+  void DisplayDealerCard(Card a_card);
+  void DisplayPlayerHand(Iterable<Card> a_hand, int a_score);
+  void DisplayDealerHand(Iterable<Card> a_hand, int a_score);
   void DisplayGameOverDealerWin();
   void DisplayGameOverPlayerWin();
   void DisplayWhenNewCard(Iterable<Card> a_dealerHand, int a_dealerScore, Iterable<Card> a_playerHand, int a_playerScore);
